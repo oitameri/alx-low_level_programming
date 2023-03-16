cc564294c6d78ff9137bb216e5a0faa435639024
oitameri <arnoldimoh@gmail.com>
2023-03-16 08:18:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  * Return: Always 0 (Success)
  */
 int main(void){
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
