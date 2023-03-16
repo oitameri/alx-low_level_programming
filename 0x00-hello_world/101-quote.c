2cfcb3349ed0d4b9e5efc1eebfdc318d241e6d61
oitameri <arnoldimoh@gmail.com>
2023-03-16 12:18:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,15 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * description: unix simplicity
- * Return 1 (Success)
  *
- **/
+ * Description: 'unix simplicity'
+ *
+ * Return 1 (Success)
+ */
 int main(void)
 {
-	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	fputs(str, stdout);
 	return (1);
 }
