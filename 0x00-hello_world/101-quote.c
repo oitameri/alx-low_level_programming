2e5fc695bee0b37a55321702a456df1a92109f4b
oitameri <arnoldimoh@gmail.com>
2023-03-16 12:09:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * main - A c program that prints a line to the standard error
  * description: unix simplicity
  * Return 1 (Success)
- */
+ *
+ **/
 int main(void)
 {
 	char * str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
