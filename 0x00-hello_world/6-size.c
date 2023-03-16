f2457a1d3cc90557a3d6be96d6ccc6e33bce8847
oitameri <arnoldimoh@gmail.com>
2023-03-16 08:13:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(){
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char a;
 	int b;
 	long int c;
