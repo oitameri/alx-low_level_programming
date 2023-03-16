36e7072b65894916ab2ae1c52efe8d33f28bba90
oitameri <arnoldimoh@gmail.com>
2023-03-16 08:25:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * prints a line to std err
+ * main - prints a line to std err
  *
- * Return: Always 0 (Success)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - 
-Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korp
+ar, 2015-10-19\n", stdout);
 	return (1);
 }
