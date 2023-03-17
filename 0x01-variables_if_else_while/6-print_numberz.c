15f5ecc39c85421fe3f10c2dfdce3738a3034e7d
oitameri <arnoldimoh@gmail.com>
2023-03-17 08:44:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry poit
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i =0;
+	while
+		(i < 10){
+			putchar(i + '0');
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
