94c3451542c87b105a0b8465642be0c87f8f862a
oitameri <arnoldimoh@gmail.com>
2023-03-17 10:44:57 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,19 +9,18 @@
 
 void trimleadingandTrailing(char *s)
 {
-        int  i, j;
+	int  i, j;
 
-        for(i = 0; s[i] == ' '|| s[i] == '\t'; i++);
-
-        for(j=0; s[i]; i++)
-        {
-                s[j++] = s[i];
+	for (i = 0; s[i] == ' '  || s[i] == '\t'; i++);
+	for (j = 0; s[i]; i++)
+	{
+		s[j++] = s[i];
         }
-        s[j] = '\0';
-        for(i = 0; s[i] != '\0'; i++)
+	s[j] = '\0';
+	for (i = 0; s[i] != '\0'; i++)
         {
-                if(s[i] != ' ' && s[i] != '\t')
-                                j = i;
+                if (s[i] != ' ' && s[i] != '\t')
+			j=i;
         }
         s[j + 1] = '\0';
 }
