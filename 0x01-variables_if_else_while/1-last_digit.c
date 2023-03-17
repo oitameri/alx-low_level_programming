e3da8ca5e102e5d2cf3fccefbc9e0741ff9d2bc5
oitameri <arnoldimoh@gmail.com>
2023-03-17 12:10:07 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,30 +5,10 @@
  * main - Entry point
  *
  * Decription: 'the last digit'
- * @parameter: trimleadingandTrailing
  *
  * Return: Always 0 (success)
  */
 
-void trimleadingandTrailing(char *s)
-{
-	int  i, j;
-
-	for (i = 0; s[i] == ' '  || s[i] == '\t';) 
-			i++;
-
-	for (j = 0; s[i]; i++)
-	{
-		s[j++] = s[i];
-	}
-	s[j] = '\0';
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] != ' ' && s[i] != '\t')
-			j = i;
-	}
-	s[j + 1] = '\0';
-}
 int main(void)
 {
 	int n;
