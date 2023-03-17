d739d882f19ec8bc87c21746f7bac4255c65a00d
oitameri <arnoldimoh@gmail.com>
2023-03-17 08:44:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: lways 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+	i = 'z';
+	while
+		(i >= 'a'){
+		putchar(c);
+		c--;
+		}
+	putchar('\n');
+	return (0);
+}
