d53b4ffbad2b9573b4acc2ecce15fd4b5ebda879
oitameri <arnoldimoh@gmail.com>
2023-03-17 08:46:22 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        int i;
+        int j;
+	int k;
+
+	i = 0;
+	while (i <= 7)
+	{
+		j = i +1;
+		while (j <= 8)
+		{
+			k = j + 1;
+			while (k <= 9)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (a < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n')
+        return (0);
+}
