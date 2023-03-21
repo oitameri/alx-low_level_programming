a4844cbb01e624cf4e3b9ec9d65844f46432c23a
oitameri <arnoldimoh@gmail.com>
2023-03-21 13:03:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- * 
- * Retur: Always 0 (Success)
+ * Description: outputting putchr
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -18,5 +18,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
